e28e521ee8d095ecb8b0fba764c145cb7ffca6ba
razalbinrafiq <razalbinrafiq@gmail.com>
2021-05-26 16:59:16 +0530

    userprofile.java

--- app/src/main/java/com/example/profittrader/UserProfile.java
+++ app/src/main/java/com/example/profittrader/UserProfile.java
@@ -4,19 +4,69 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
+
+    TextView nameTextView,emailTextView,phoneTextView;
+    Button passwordEditButton;
+    String check_ID;
+    String idString,nameString,emailString,phoneString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+
+        nameTextView=(TextView)findViewById(R.id.nameTextView);
+        emailTextView=(TextView)findViewById(R.id.emailIDTextView) ;
+        phoneTextView=(TextView)findViewById(R.id.phoneTextView) ;
+
+
+        SharedPreferences loginDetails =  getSharedPreferences("loginDetails", MODE_PRIVATE);
+        check_ID = loginDetails.getString("id","0");
+
+        DatabaseReference fb_to_read = FirebaseDatabase.getInstance().getReference("users/"+check_ID+"/userDetails");
+
+        fb_to_read.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<String> list=new ArrayList<String>();
+                for (DataSnapshot dsp : snapshot.getChildren()){
+                   idString=snapshot.getKey().toString();
+                }
+
+                nameString=snapshot.child("name").getValue(String.class).toUpperCase();
+                emailString=snapshot.child("emailId").getValue(String.class);
+                phoneString=snapshot.child("mobile").getValue(String.class);
+
+                nameTextView.setText(nameString);
+                emailTextView.setText(emailString);
+                phoneTextView.setText(phoneString);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
